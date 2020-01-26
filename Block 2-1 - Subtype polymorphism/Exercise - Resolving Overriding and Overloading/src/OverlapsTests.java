2b6e11c77abb63b009a60513c1ba1b0880d59fa3
niklasbroberg <niklas.broberg@gmail.com>
2020-01-26 19:24:32 +0100

    Adding test file

---
+++ Block 2-1 - Subtype polymorphism/Exercise - Resolving Overriding and Overloading/src/OverlapsTests.java
@@ -0,0 +1,10 @@
+public class OverlapsTests {
+
+    public static void main(String[] args){
+        Square square = new Square(10,10);
+        Triangle triangle = new Triangle(10,20);
+        Rectangle rectangle = new Rectangle(10,30);
+
+        // your code here
+    }
+}
